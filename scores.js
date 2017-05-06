3d86d9a0dfdec2b873a66a88c1601baa74e737e5
Mateusz Kwaśniak <203923@edu.p.lodz.pl>
2017-05-06 10:20:46 +0200

    Do some code refactor

--- scores.js
+++ scores.js
@@ -1,4 +1,4 @@
-function gatherScores(scoreInputName, sumInputName) {
+function score(scoreInputName, sumInputName) {
     var scores = document.getElementsByName(scoreInputName);
     var sum = 0;
     for(var i = 0; i < scores.length; ++i) {
@@ -11,22 +11,25 @@ function gatherScores(scoreInputName, sumInputName) {
 }
 
 function calculate() {
-    document.getElementById("p1_final").value = 
-        gatherScores("p1g1", "p1g1_score") +
-        gatherScores("p1g2", "p1g2_score") +
-        gatherScores("p1g3", "p1g3_score");
-    document.getElementById("p2_final").value = 
-        gatherScores("p2g1", "p2g1_score") +
-        gatherScores("p2g2", "p2g2_score") +
-        gatherScores("p2g3", "p2g3_score");
+    var player1 = score("p1g1", "p1g1_score") + score("p1g2", "p1g2_score") + score("p1g3", "p1g3_score");
+    var player2 = score("p2g1", "p2g1_score") + score("p2g2", "p2g2_score") + score("p2g3", "p2g3_score");
 
-    var winner = "p1_final";
-    var loser = "p2_final";
-    if(document.getElementById("p2_final").value > document.getElementById("p1_final").value) {
-        loser = [winner, winner = loser][0];
+    document.getElementById("p1_final").value = player1;
+    document.getElementById("p2_final").value = player2;
+
+    if(player1 != player2) {
+        var loser = "p2_final";
+        var winner = "p1_final";
+        if(player2 > player1) { loser = [winner, winner = loser][0]; }
+        document.getElementById(winner).style.color = "#eee";
+        document.getElementById(winner).style.backgroundColor = "#33ff33";
+        document.getElementById(loser).style.color = "#eee";
+        document.getElementById(loser).style.backgroundColor = "#ff3333";
     }
-    document.getElementById(winner).style.color = "#eee";
-    document.getElementById(winner).style.backgroundColor = "#33ff33";
-    document.getElementById(loser).style.color = "#eee";
-    document.getElementById(loser).style.backgroundColor = "#ff3333";
+    else {
+        document.getElementById("p1_final").style.color = "";
+        document.getElementById("p1_final").style.backgroundColor = "";
+        document.getElementById("p2_final").style.color = "";
+        document.getElementById("p2_final").style.backgroundColor = "";
+    }
 }
